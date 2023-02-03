2af081b7106123cb9c3fe4a8c59cf933f830c419
GallaDindorogo <107935917+GallaDindorogo@users.noreply.github.com>
2023-02-03 02:44:20 +0100

    add

--- src/components/MyBooks/ContactForm/ContactForm.jsx
+++ src/components/MyBooks/ContactForm/ContactForm.jsx
@@ -1,70 +1,64 @@
 import styles from './contactForm.module.scss';
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import initialState from './initialState';
 
-class ContactForm extends Component {
-  state = { ...initialState };
+const ContactForm = ({ onSubmit }) => {
+  const [state, setState] = useState({ ...initialState });
 
-  handleSubmit = e => {
-    e.preventDefault();
-    const { onSubmit } = this.props;
-    onSubmit(this.state);
-    this.resetForm();
-  };
+  const { name, number } = state;
 
-  resetForm = () => {
-    this.setState({ ...initialState });
-  };
-
-  handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
     const { name, value } = target;
-    this.setState({
-      [name]: value,
+    setState(prevState => {
+      return { ...prevState, [name]: value };
     });
   };
 
-  render() {
-    const { handleChange, handleSubmit } = this;
-    const { name, number } = this.state;
-    return (
-      <form className={styles.form} onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <label>Name</label>
-          <input
-            className={styles.inputName}
-            name="name"
-            value={name}
-            onChange={handleChange}
-            placeholder="Name"
-            type="text"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label>Number</label>
-          <input
-            className={styles.inputNumber}
-            name="number"
-            value={number}
-            onChange={handleChange}
-            placeholder="tel.number"
-            type="tel"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </div>
-        <button type="submit">Add contact</button>
-      </form>
-    );
-  }
-}
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ name, number });
+    setState({ ...initialState });
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <div className={styles.formGroup}>
+        <label>Name</label>
+        <input
+          className={styles.inputName}
+          name="name"
+          value={name}
+          onChange={handleChange}
+          placeholder="Name"
+          type="text"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label>Number</label>
+        <input
+          className={styles.inputNumber}
+          name="number"
+          value={number}
+          onChange={handleChange}
+          placeholder="tel.number"
+          type="tel"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </div>
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
 
 export default ContactForm;
 
 ContactForm.propTypes = {
   onSubmit: PropTypes.func.isRequired,
 };
+
--- src/components/MyBooks/MyBooks.jsx
+++ src/components/MyBooks/MyBooks.jsx
@@ -1,63 +1,26 @@
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { nanoid } from 'nanoid';
 
 import styles from './my-books.module.scss';
 import ContactList from './ContactList/ContactList';
 import ContactFilter from './ContactFilter/ContactFilter';
-// import items from 'components/items';
+
 import ContactForm from './ContactForm/ContactForm';
 
-class MyBooks extends Component {
-  state = {
-    items: [],
-    filter: '',
-  };
-
-  componentDidMount() {
+const MyBooks = () => {
+  const [items, setItems] = useState(() => {
     const items = JSON.parse(localStorage.getItem('my-contact'));
-    if (items?.length) {
-      //items && items.length
-      this.setState({ items });
-    }
-  }
+    return items ? items : [];
+  });
+  const [filter, setFilter] = useState('');
 
-  componentDidUpdate(prevProps, prevState) {
-    const { items } = this.state;
-    if (prevState.items.length !== items.length) {
-      localStorage.setItem('my-contact', JSON.stringify(items));
-    }
-  }
+  useEffect(() => {
+    localStorage.setItem('my-contact', JSON.stringify(items));
+  }, [items]);
 
-  addContact = ({ name, number }) => {
-    if (this.isDublicate(name, number)) {
-      return alert(`${name} tel.${number} is already in contacts`);
-    }
-    this.setState(prevState => {
-      const { items } = prevState;
-      const newContact = {
-        id: nanoid(),
-        name,
-        number,
-      };
-      return { items: [newContact, ...items], name: '', number: '' };
-    });
-  };
-
-  removeContact = id => {
-    this.setState(({ items }) => {
-      const newContacts = items.filter(item => item.id !== id);
-      return { items: newContacts };
-    });
-  };
-
-  handleFilter = ({ target }) => {
-    this.setState({ filter: target.value });
-  };
-
-  isDublicate(name, number) {
+  const isDublicate = (name, number) => {
     const normalizedName = name.toLowerCase();
     const normalizedNumber = number.toLowerCase();
-    const { items } = this.state;
     const contact = items.find(({ name, number }) => {
       return (
         name.toLowerCase() === normalizedName &&
@@ -65,10 +28,29 @@ class MyBooks extends Component {
       );
     });
     return Boolean(contact);
-  }
+  };
 
-  getVisibleContacts() {
-    const { filter, items } = this.state;
+  const addContact = ({ name, number }) => {
+    if (isDublicate(name, number)) {
+      return alert(`${name} tel.${number} is already in contacts`);
+    }
+    setItems(prevItems => {
+      const newContact = {
+        id: nanoid(),
+        name,
+        number,
+      };
+      return [newContact, ...prevItems];
+    });
+  };
+
+  const removeContact = id => {
+    setItems(prevItems => prevItems.filter(item => item.id !== id));
+  };
+
+  const handleFilter = ({ target }) => setFilter(target.value);
+
+  const getVisibleContacts = () => {
     if (!filter) {
       return items;
     }
@@ -80,33 +62,28 @@ class MyBooks extends Component {
       );
     });
     return resalt;
-  }
+  };
 
-  render() {
-    const { addContact, handleFilter, removeContact } = this;
-    const contacts = this.getVisibleContacts();
+  const filtredContacts = getVisibleContacts();
+  // const isContacts = Boolean(filtredContacts.length);
 
-    return (
-      <div>
-        <h3>Phonebook</h3>
-        <div className={styles.wrapper}>
-          <div className={styles.block}>
-            <h4>Add new contact</h4>
-            <ContactForm onSubmit={addContact} />
-          </div>
-          <div className={styles.block}>
-            <h4 className={styles.titleContacts}>Contacts:</h4>
-            <h4 className={styles.allContacts}>
-              All contacts: {contacts.length}
-            </h4>
-            <ContactFilter handleChange={handleFilter} />
+  return (
+    <div>
+      <h3>Phonebook</h3>
+      <div className={styles.wrapper}>
+        <div className={styles.block}>
+          <h4>Add new contact</h4>
+          <ContactForm onSubmit={addContact} />
+        </div>
+        <div className={styles.block}>
+          <h4 className={styles.titleContacts}>Contacts:</h4>
+          <h4 className={styles.allContacts}>All contacts: {items.length}</h4>
+          <ContactFilter handleChange={handleFilter} />
 
-            <ContactList removeContact={removeContact} items={contacts} />
-          </div>
+          <ContactList removeContact={removeContact} items={filtredContacts} />
         </div>
       </div>
-    );
-  }
-}
-
+    </div>
+  );
+};
 export default MyBooks;
